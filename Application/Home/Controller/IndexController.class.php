d9cffb150b2adf4ee2cfd5a68f9974c0705f882d
chenlei <chenlei2015@yeah.net>
2016-11-28 23:03:07 +0800

    ok

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -6,7 +6,20 @@ class IndexController extends Controller
 {
     public function index()
     {
-        $wechat=new WechatBus();
-        $wechat->checkSignature();
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+
+        $token = "relay";
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+        if( $tmpStr == $signature ){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
