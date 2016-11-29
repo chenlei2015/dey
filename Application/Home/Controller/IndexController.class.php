ba30fb10c2b53f05fa69ee854cff2944ccc1c37c
chenlei <chenlei2015@yeah.net>
2016-11-29 08:48:22 +0800

    ok

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -23,7 +23,34 @@ class IndexController extends Controller
             echo  $echostr;
             exit;
         }else{
-            $this->reponseMsg();
+            return $this->reponseMsg();
         }
     }
+
+    public function reponseMsg(){
+        //1.获取到微信推送过来post数据（xml格式）
+        $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
+        //2.处理消息类型，并设置回复类型和内容
+        $postObj = simplexml_load_string( $postArr );
+        //判断该数据包是否是订阅的事件推送
+        if( strtolower( $postObj->MsgType) == 'event') {
+            //如果是关注 subscribe 事件
+            if (strtolower($postObj->Event == 'subscribe')) {
+                //回复用户消息(纯文本格式)
+                $toUser = $postObj->FromUserName;
+                $fromUser = $postObj->ToUserName;
+                $time = time();
+                $msgType = 'text';
+                $content = '欢迎关注我们的微信公众账号' . $postObj->FromUserName . '-' . $postObj->ToUserName;
+                $template = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[%s]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							</xml>";
+                $info = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
+                echo $info;
+            }
+        }
 }
