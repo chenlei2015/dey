3734411ad2098a31e9ab3282427836733d10ba18
chenlei <chenlei2015@yeah.net>
2016-11-29 09:04:52 +0800

    ok

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -27,13 +27,14 @@ class IndexController extends Controller
         }
     }
 
-    public function reponseMsg(){
+    public function reponseMsg()
+    {
         //1.获取到微信推送过来post数据（xml格式）
         $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
         //2.处理消息类型，并设置回复类型和内容
-        $postObj = simplexml_load_string( $postArr );
+        $postObj = simplexml_load_string($postArr);
         //判断该数据包是否是订阅的事件推送
-        if( strtolower( $postObj->MsgType) == 'event') {
+        if (strtolower($postObj->MsgType) == 'event') {
             //如果是关注 subscribe 事件
             if (strtolower($postObj->Event == 'subscribe')) {
                 //回复用户消息(纯文本格式)
@@ -53,4 +54,6 @@ class IndexController extends Controller
                 echo $info;
             }
         }
+
+    }
 }
