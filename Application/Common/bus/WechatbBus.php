56b3c2553f4161eb06ee96a402776e79af4e1afa
chenlei <chenlei2015@yeah.net>
2016-11-29 08:25:30 +0800

    ok

--- Application/Common/bus/WechatbBus.php
+++ Application/Common/bus/WechatbBus.php
@@ -3,20 +3,7 @@ namespace Common\bus;
 class WechatBus{
     public function checkSignature()
     {
-        $signature = $_GET["signature"];
-        $timestamp = $_GET["timestamp"];
-        $nonce = $_GET["nonce"];
 
-        $token = "relay";
-        $tmpArr = array($token, $timestamp, $nonce);
-        sort($tmpArr, SORT_STRING);
-        $tmpStr = implode( $tmpArr );
-        $tmpStr = sha1( $tmpStr );
 
-        if( $tmpStr == $signature ){
-            return true;
-        }else{
-            return false;
-        }
     }
 }
